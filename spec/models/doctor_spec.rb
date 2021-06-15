61143483aececcdb7401da1c36c5118932a90aae
deebot10 <dee1jhill@gmail.com>
2021-06-15 09:22:27 -0600

    fix test

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Doctor do
   describe 'relationships' do
     it { should belong_to(:hospital) }
-    it { should have_many(:on_call) }
-    it { should have_many(:patients).through(:on_call) }
+    it { should have_many(:on_calls) }
+    it { should have_many(:patients).through(:on_calls) }
   end
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Patient do
   describe 'relationships' do
-    it { should have_many(:on_call) }
-    it { should have_many(:doctors).through(:on_call) }
+    it { should have_many(:on_calls) }
+    it { should have_many(:doctors).through(:on_calls) }
   end
 end
