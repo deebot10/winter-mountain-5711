b8cf244532efca23dfbd9fade65194cd7c9caf15
deebot10 <dee1jhill@gmail.com>
2021-06-15 09:43:10 -0600

    Create doctor controller

--- spec/features/doctor/show_spec.rb
+++ spec/features/doctor/show_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'Doctor show' do
   before(:each) do
     @hospital = Hospital.create!(name: 'Seattle Grace')
     
-    @doctor = @hospital.doctor.create!(name: 'J.D Dorian', specialty: 'General', education: 'Phoniex Online')
+    @doctor = @hospital.doctors.create!(name: 'J.D Dorian', specialty: 'General', university: 'Phoniex Online')
 
-    @patient_1 = @doctor.patient.create!(name: 'Kyle Lowry', age: 33)
-    @patient_2 = @doctor.patient.create!(name: 'Zack Levine', age: 25)
-    @patient_3 = @doctor.patient.create!(name: 'Charles Barkly', age: 42)
+    @patient_1 = @doctor.patients.create!(name: 'Kyle Lowry', age: 33)
+    @patient_2 = @doctor.patients.create!(name: 'Zack Levine', age: 25)
+    @patient_3 = @doctor.patients.create!(name: 'Charles Barkly', age: 42)
 
-    
+    visit doctor_path(@doctor)
   end    
     # User Story 1, Doctors Show Page
     # ​
@@ -22,6 +22,9 @@ RSpec.describe 'Doctor show' do
     # - university where they got their doctorate
     # And I see the name of the hospital where this doctor works
     # And I see the names of all of the patients this doctor has
-  it '' do
+  it 'shows the doctor with attributes' do
+    expect(page).to have_content(@doctor.name)  
+    expect(page).to have_content(@doctor.specialty)  
+    expect(page).to have_content(@doctor.university)  
   end
 end
